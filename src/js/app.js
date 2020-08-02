fcfd008348db43965385155cb0e38c4197d1084a
Karthik <karthikeyan.rajendran@health.telstra.com>
2020-08-03 08:53:53 +1000

    Config update

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,23 @@
 if (module.hot) {
   module.hot.accept();
 }
+import { today, message, noteConfig, storeKey, SetContent } from './config';
+import './date-selection';
 import MediumEditor from 'medium-editor/dist/js/medium-editor';
-import { storeKey, readNotebook, updateNotebook } from './store';
-import './welcome';
 
-const editor = new MediumEditor('.editable', {
-  toolbar: {
-    buttons: ['bold', 'italic', 'underline', 'strikethrough', 'quote', 'anchor', 'orderedlist', 'unorderedlist', 'pre', 'removeFormat', 'outdent', 'indent', 'h2', 'h3', 'removeFormat'],
-  },
-  placeholder: {
-    text: 'Start writing...',
-  },
-  buttonLabels: 'fontawesome',
-  paste: {
-    cleanPastedHTML: true,
-    forcePlainText: false
-  }
-});
+import { readNotebook, updateNotebook } from './store';
+import './quotes';
+
+const editor = new MediumEditor('.editable', noteConfig);
 
 editor.subscribe('editableInput', function (event, editable) {
   let content = editable.innerHTML;
   updateNotebook(storeKey, JSON.stringify(content));
 });
+
+// Rock n Roll
+
+SetContent('yx-message', message);
+SetContent('yx-date-value', today);
 
 readNotebook(storeKey, editor);
---
+++ src/js/config.js
@@ -0,0 +1,58 @@
+export const date = new Date();
+export const storeKey = "yanxdrafts";
+export const quoteObjKey = "yanxquote";
+export const cachedQKey = "yanxcachedquote"
+
+const notesTweeters = [
+  "Start scratching",
+  "Make a dent",
+  "Fill me with words",
+  "Don't forget that",
+];
+const placeholderText = notesTweeters[date.getDay() % notesTweeters.length];
+export const noteConfig = {
+  toolbar: {
+    buttons: ['bold', 'underline', 'strikethrough', 'anchor', 'orderedlist', 'pre', 'outdent', 'indent', 'h3'],
+  },
+  placeholder: {
+    text: placeholderText,
+  },
+  targetCheckbox: true,
+  buttonLabels: 'fontawesome',
+  paste: {
+    cleanPastedHTML: true,
+    forcePlainText: false,
+    cleanTags: ['meta'],
+    cleanAttrs: ['class', 'style', 'dir'],
+  }
+};
+
+export const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const adjectives = [
+  "an awesome",
+  "an incredible",
+  "a thoughtful",
+  "a dashing",
+  "an amazing"
+];
+
+export const today = date.getDate() + ' ' + months[date.getMonth()];
+export const message = adjectives[date.getDay() % adjectives.length];
+
+export const SetContent = (eleID, content) => {
+  document.getElementById(eleID).innerHTML = content;
+}
