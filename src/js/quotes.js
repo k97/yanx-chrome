4258f756338d305ca5ba7c9413b92de7b7b0e904
Karthik <karthikeyan.rajendran@health.telstra.com>
2020-08-03 08:54:46 +1000

    Quotes feature rollout

---
+++ src/js/quotes.js
@@ -0,0 +1,46 @@
+import { date, quoteObjKey, cachedQKey,  SetContent } from './config';
+import { quoteDump } from './quote-dump';
+
+const loadQuoteSource = () => {
+    console.log('Calling Dump...')
+    const quote = quoteDump[date.getDay() % quoteDump.length]
+    const cacheSetter = quoteObjKey + ' - ' + date.toDateString();
+    setQuoteContent(quote);
+    localStorage.setItem(cachedQKey, JSON.stringify(cacheSetter));
+}
+
+const getLocalQuote = () => {
+    let cachedQuoteObj = localStorage.getItem(quoteObjKey);
+    if (cachedQuoteObj) {
+        let quoteObj = JSON.parse(cachedQuoteObj);
+        setQuoteContent(quoteObj);
+    } else {
+        console.log('Init subsequent time quote call');
+        loadQuoteSource();
+    }
+}
+
+const setQuoteContent = (obj) => {
+    localStorage.setItem(quoteObjKey, JSON.stringify(obj));
+    SetContent('yx-quote-text', obj.quote);
+    SetContent('yx-quote-author', obj.author);
+    document.getElementById('yx-quote-author').href = 'https://google.com/search?q=' + obj.author + '+quotes';
+}
+
+const initQuote = () => {
+    const cacheKeyVal = localStorage.getItem(cachedQKey);
+    const savedDate = cacheKeyVal ? JSON.parse(cacheKeyVal).split(' - ') : null;
+
+    // A new quote everyday :)
+    if (savedDate && savedDate[1] == date.toDateString()) {
+        console.log('Init local quote');
+        getLocalQuote();
+    } else {
+        localStorage.removeItem(cacheKeyVal);
+        loadQuoteSource();
+    }
+}
+
+
+initQuote();
+
