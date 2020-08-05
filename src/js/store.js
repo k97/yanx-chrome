c5a78c5e5b0361358d07e09c667076e894d5b473
Karthik <karthikeyan.rajendran@health.telstra.com>
2020-08-05 23:34:17 +1000

    credits info update

--- src/js/store.js
+++ src/js/store.js
@@ -2,9 +2,12 @@ import { date, storeKey, noteDateRef } from './config';
 
 var globalStore;
 
-export const initNotebook = (store, dateRef) => {
+export const getNotebook = (store, dateRef) => {
   let note;
+
+  //If store is passed from storage
   if (store) {
+    store = clearOldNotes(store);
     globalStore = store;
     note = store[dateRef]
   } else {
@@ -25,19 +28,39 @@ export const updateNotebook = (data) => {
   chrome.storage.sync.set(obj);
 }
 
-export const readNotebook = (datekey, editorRef) => {
+export const initNotebook = (datekey, editorRef) => {
   if (!chrome.storage) {
     let result = localStorage.getItem(storeKey);
     let storeBlob = JSON.parse(result);
-    let data = initNotebook(storeBlob, datekey);
+    let data = getNotebook(storeBlob, datekey);
     editorRef.setContent(data, 0);
     return
   }
 
   chrome.storage.sync.get([datekey], function (result) {
-    let data = initNotebook(result, datekey);
+    let data = getNotebook(result, datekey);
     editorRef.setContent(data, 0);
   });
 }
 
+export const clearOldNotes = (store) => {
+  let updatedStore = {};
 
+  let activeDateKeys = [...Array(7)].map((val, i) => {
+    const d = new Date();
+    d.setDate(d.getDate() - i);
+    return noteDateRef + d.toDateString().split(' ').join('-');
+  });
+
+  const keys = Object.keys(store);
+  for (let key of keys) {
+    for (let i = 0; i < activeDateKeys.length; i++) {
+      if (key === activeDateKeys[i]) {
+        Object.assign(updatedStore, {
+          [key]: store[key]
+        });
+      }
+    }
+  }
+  return updatedStore;
+}
